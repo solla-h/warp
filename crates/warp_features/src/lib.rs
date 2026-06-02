c9ccb0b885d6a52005a00f09af7bb370de340e4f
Pei Li <pei@warp.dev>
2026-06-02 17:10:00 -0400

    enable cross-window tab dragging in preview (#12046)
    This PR enables tab dragging in preview. There are two main issues:
    1. Dragging a tab out and in repeatedly in the same mousedown doesn't
    work: https://github.com/warpdotdev/warp/pull/12044
    2. @acarl005 reported a random crash on Windows. I tried using a VM to
    reproduce but have been unsuccessful.
    
    #1 is not trivial to fix, and I'm deliberately scoping out of Preview.
    I'll take another pass before considering this feature for Stable.
    
    I'm hoping to leverage Preview users to hone in on #2, but will continue
    to attempt to reproduce until the Preview release on Thursday.

--- crates/warp_features/src/lib.rs
+++ crates/warp_features/src/lib.rs
@@ -948,7 +948,6 @@ pub const DOGFOOD_FLAGS: &[FeatureFlag] = &[
     FeatureFlag::LocalDockerSandbox,
     #[cfg(not(windows))]
     FeatureFlag::SshRemoteServer,
-    FeatureFlag::DragTabsToWindows,
     FeatureFlag::RemoteCodebaseIndexing,
     FeatureFlag::GroupedTabs,
     FeatureFlag::AsyncFind,
@@ -956,7 +955,10 @@ pub const DOGFOOD_FLAGS: &[FeatureFlag] = &[
 
 /// Features enabled for feature preview build users (e.g.: Friends of Warp).
 /// All PREVIEW_FLAGS are also automatically added to dogfood builds (WarpDev).
-pub const PREVIEW_FLAGS: &[FeatureFlag] = &[];
+pub const PREVIEW_FLAGS: &[FeatureFlag] = &[
+    #[cfg(target_os = "macos")]
+    FeatureFlag::DragTabsToWindows,
+];
 
 /// Features enabled for all release builds (i.e.: everything but WarpLocal).
 /// NOTE: if you are promoting a feature from Preview to launch, you'll likely
